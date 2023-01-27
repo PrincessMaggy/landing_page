5a5c7182ea072021b8695ce9f79f53f0f746bba4
princessmaggy <edoziemagdalene@gmail.com>
2023-01-27 10:45:40 +0100

    started styling nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import {BiMenuAltRight} from 'react-icons/bi';
+import {AiOutlineClose} from 'react-icons/ai';
 import {Link} from 'react-router-dom';
 
 function Nav() {
+const [menuOpen, menuClose] = useState(false)
+const menuToggle = () =>{
+    menuClose((p) => !p)
+}
+
+
   return (
-    <div>
-        <h1><Link id="logo" to="/newapp">New app</Link></h1>
+    <div className='Nav'>
+         <div className='navContainer'>
+            <h2><Link id="logo" to="/newapp" className='navlink'>NewApp</Link> </h2>
+         <nav>
+            <Link  className='navlink' to="/newapp">About Us</Link> 
+            <Link  className='navlink' to="/newapp">Careers</Link> 
+            <Link  className='navlink' to="/newapp">Products</Link> 
+            <Link  className='navlink' to="/newapp">Contact Us</Link> 
+            <div>
+                <button className='navbtn'>Get Started</button>
+            </div>
+         </nav>
+         <div className='toggle'>
+         {!menuOpen?  (<BiMenuAltRight onClick={menuToggle}/>) : (<AiOutlineClose  onClick={menuToggle} />) }
+           
+         </div>
+        </div>
     </div>
     
   )
