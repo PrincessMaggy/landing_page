e41f3c18154ab5e37af56f58e544ff7f59b2d96c
princessmaggy <edoziemagdalene@gmail.com>
2023-01-27 08:26:01 +0100

    started project

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import Nav from '../components/Nav';
+
+function Home() {
+  return (
+    <div className='home'>
+        <Nav/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home;
