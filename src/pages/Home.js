deaa9fa72c66e0b2857a0742a78e1e2b820daec8
PrincessMaggy <edoziemagdalene@gmail.com>
2023-01-29 23:17:49 +0100

    hero-section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Nav from '../components/Nav';
+// import heroimg from '../img/home-hero.png'
+import Join from '../components/Join';
+
 
 function Home() {
   return (
-    <div className='home'>
-        <Nav/>
-        <Footer/>
+    <div>
+      <div className='home'>
+        <div className="main">
+            <Nav/>
+            <div className="hero">
+              <div className="hero-left">
+                <h1>Lorem ipsum dolor sit amet, consectetur.</h1>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.dapibus ultricies neque sed rhoncus. Integer consequat aliquet aliquet. Quisque aliquam feugiat mauris, eu fringilla dolor eleifend id. Proin nibh dui, dictum nec enim eu, posuere dapibus nulla. Proin interdu
+          m condimentum sollicitudin. Ut eros nunc, mattis et mattis nec, viverra eu enim. Aenean fermentum egestas eros. Pellentesque non tellus nisi. Duis convallis ex id dui viverra molestie id at dolor. Nam et libero at </p>
+              
+              <button className='navbtn'>Get Started</button>
+          </div>
+            <div className="hero-right">
+              </div>
+            </div>
+        </div>
+          <Join/>
+          <Footer/>
+      </div>
     </div>
   )
 }
